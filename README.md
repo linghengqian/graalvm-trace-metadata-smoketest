8d7ffe6ad0dfb3b48e008d461ef9e3f4b8306b2e
linghengqian <linghengqian@outlook.com>
2022-11-17 23:48:54 +0800

    Add JJWT's GraalVM reachability metadata.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # graalvm-trace-metadata-smoketest
+
+- This warehouse is my personal temporary warehouse so that I can record the dynamics of a series of issues in time. All
+  unit test designs should be submitted to https://github.com/oracle/graalvm-reachability-metadata as much as possible.
+
+- `org.apache.shardingsphere:shardingsphere-proxy-bootstrap:5.2.1`,
+  at https://github.com/apache/shardingsphere/issues/21347 .
+- `com.github.luben:zstd-jni:1.5.2`, at https://github.com/luben/zstd-jni/issues/115 .
+- `io.jsonwebtoken:jjwt-api:0.11.5`, at https://github.com/jwtk/jjwt/issues/637 .
+- `io.jsonwebtoken:jjwt-impl:0.11.5`, at https://github.com/jwtk/jjwt/issues/637 .
+- `org.apache.skywalking:java-agent:8.13.0`, at https://github.com/apache/skywalking/discussions/7640 .
+- `org.ehcache:ehcache:3.10.4`, at https://github.com/ehcache/ehcache3/issues/2992 .
+- `io.vertx:vertx-core:4.3.3`( Or `io.vertx:vertx-core:4.3.4` ?) ,at https://github.com/netty/netty/issues/11369 .
---
+++ jjwt-impl/src/test/java/com/lingh/JjwtImplTests.java
@@ -0,0 +1,74 @@
+package com.lingh;
+
+import io.jsonwebtoken.CompressionCodecs;
+import io.jsonwebtoken.JwtParserBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.io.Encoders;
+import io.jsonwebtoken.security.Keys;
+import org.junit.jupiter.api.Test;
+
+import javax.crypto.SecretKey;
+import java.security.KeyPair;
+import java.util.Date;
+import java.util.UUID;
+
+public class JjwtImplTests {
+    @Test
+    void testSignedJWTs() {
+        SecretKey firstKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+        String firstCompactJws = Jwts.builder().setSubject("Joe").signWith(firstKey).compact();
+        assert Jwts.parserBuilder().setSigningKey(firstKey).build().parseClaimsJws(firstCompactJws).getBody().getSubject().equals("Joe");
+        String secretString = Encoders.BASE64.encode(firstKey.getEncoded());
+        SecretKey secondKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretString));
+        String secondCompactJws = Jwts.builder().setSubject("Joe").signWith(secondKey).compact();
+        assert Jwts.parserBuilder().setSigningKey(secondKey).build().parseClaimsJws(secondCompactJws).getBody().getSubject().equals("Joe");
+        KeyPair keyPair = Keys.keyPairFor(SignatureAlgorithm.RS256);
+        String thirdCompactJws = Jwts.builder().setSubject("Joe").signWith(keyPair.getPrivate()).compact();
+        assert Jwts.parserBuilder().setSigningKey(keyPair.getPublic()).build().parseClaimsJws(thirdCompactJws).getBody().getSubject().equals("Joe");
+    }
+
+    @Test
+    void testCreatingAJWS() {
+        Date firstDate = new Date();
+        Date secondDate = new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000L);
+        String uuidString = UUID.randomUUID().toString();
+        SecretKey firstKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+        String firstCompactJws = Jwts.builder()
+                .setSubject("Joe")
+                .setHeaderParam("kid", "myKeyId")
+                .setIssuer("Aaron")
+                .setAudience("Abel")
+                .setExpiration(secondDate)
+                .setNotBefore(firstDate)
+                .setIssuedAt(firstDate)
+                .setId(uuidString)
+                .claim("exampleClaim", "Adam")
+                .signWith(firstKey, SignatureAlgorithm.HS256)
+                .compressWith(CompressionCodecs.GZIP)
+                .compact();
+        JwtParserBuilder jwtParserBuilder = Jwts.parserBuilder().setAllowedClockSkewSeconds(3 * 60);
+        assert jwtParserBuilder.setSigningKey(firstKey).build().parseClaimsJws(firstCompactJws).getBody().getSubject().equals("Joe");
+        jwtParserBuilder.requireSubject("Joe").setSigningKey(firstKey).build().parseClaimsJws(firstCompactJws);
+        jwtParserBuilder.requireIssuer("Aaron").setSigningKey(firstKey).build().parseClaimsJws(firstCompactJws);
+        jwtParserBuilder.requireAudience("Abel").setSigningKey(firstKey).build().parseClaimsJws(firstCompactJws);
+        jwtParserBuilder.requireExpiration(secondDate).setSigningKey(firstKey).build().parseClaimsJws(firstCompactJws);
+        jwtParserBuilder.requireNotBefore(firstDate).setSigningKey(firstKey).build().parseClaimsJws(firstCompactJws);
+        jwtParserBuilder.requireIssuedAt(firstDate).setSigningKey(firstKey).build().parseClaimsJws(firstCompactJws);
+        jwtParserBuilder.requireId(uuidString).setSigningKey(firstKey).build().parseClaimsJws(firstCompactJws);
+        jwtParserBuilder.require("exampleClaim", "Adam").setSigningKey(firstKey).build().parseClaimsJws(firstCompactJws);
+    }
+
+    @Test
+    void testCompression(){
+        SecretKey firstKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+        String firstCompactJws = Jwts.builder().setSubject("Joe").signWith(firstKey).compressWith(CompressionCodecs.DEFLATE).compact();
+        assert Jwts.parserBuilder().setSigningKey(firstKey).build().parseClaimsJws(firstCompactJws).getBody().getSubject().equals("Joe");
+        SecretKey secondKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+        String secondCompactJws = Jwts.builder().setSubject("Joe").signWith(secondKey).compressWith(CompressionCodecs.GZIP).compact();
+        assert Jwts.parserBuilder().setSigningKey(secondKey).build().parseClaimsJws(secondCompactJws).getBody().getSubject().equals("Joe");
+    }
+
+
+}
