8c3da12706fb20259fdd9e7c62551062e65d8cd0
linghengqian <linghengqian@outlook.com>
2022-11-20 17:27:14 +0800

    - HttpServerVerticle use lambda.

--- vertx-core/src/test/java/com/lingh/http/sharing/HttpServerVerticle.java
+++ vertx-core/src/test/java/com/lingh/http/sharing/HttpServerVerticle.java
@@ -5,11 +5,10 @@ import io.vertx.core.AbstractVerticle;
 public class HttpServerVerticle extends AbstractVerticle {
     @Override
     public void start() {
-        vertx.createHttpServer().requestHandler(req -> {
-            req.response()
-                    .putHeader("content-type", "text/html")
-                    .end("<html><body><h1>Hello from " +  this + "</h1></body></html>");
-        }).listen(8293);
+        vertx.createHttpServer().requestHandler(req -> req.response()
+                        .putHeader("content-type", "text/html")
+                        .end("<html><body><h1>Hello from " + this + "</h1></body></html>"))
+                .listen(8293);
     }
 }
 
