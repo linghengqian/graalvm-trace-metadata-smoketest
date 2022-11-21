b980b98f9b2b1fda9c3cb6517a455c8f78ef1091
linghengqian <linghengqian@outlook.com>
2022-11-22 06:11:07 +0800

    - make ZstdJniTest be simple.

--- zstd-jni/src/test/java/com/lingh/ZstdJniTest.java
+++ zstd-jni/src/test/java/com/lingh/ZstdJniTest.java
@@ -107,7 +107,8 @@ public class ZstdJniTest {
             offset = offset + read;
         }
         zis.close();
-        assertThat(Arrays.toString(input)).isEqualTo("[97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122]");
-        assertThat(Arrays.toString(input)).isEqualTo("[97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122]");
+        String s = "[97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122]";
+        assertThat(Arrays.toString(input)).isEqualTo(s);
+        assertThat(Arrays.toString(input)).isEqualTo(s);
     }
 }
