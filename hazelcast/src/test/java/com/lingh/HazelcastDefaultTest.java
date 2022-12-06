e5e833ba6291cadc762c52b8644e0eb429a95190
linghengqian <linghengqian@outlook.com>
2022-12-07 04:25:06 +0800

    - change hazelcast test style.

--- hazelcast/src/test/java/com/lingh/HazelcastDefaultTest.java
+++ hazelcast/src/test/java/com/lingh/HazelcastDefaultTest.java
@@ -26,4 +26,18 @@ public class HazelcastDefaultTest {
             hazelcastInstance.shutdown();
         });
     }
+
+    @Test
+    void testJCacheOrigin() {
+        HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance(new Config());
+        CachingProvider cachingProvider = Caching.getCachingProvider(HazelcastCachingProvider.class.getName());
+        CacheManager cacheManager = cachingProvider.getCacheManager();
+        CompleteConfiguration<String, String> config = new MutableConfiguration<String, String>().setTypes(String.class, String.class);
+        Cache<String, String> cache = cacheManager.createCache("example", config);
+        cache.put("world", "Hello World");
+        assertThat(cache.get("world")).isEqualTo("Hello World");
+        assertThat(cacheManager.getCache("example", String.class, String.class)).isNotNull();
+        hazelcastInstance.shutdown();
+    }
 }
+
