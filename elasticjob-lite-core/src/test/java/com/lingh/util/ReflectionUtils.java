bf29dc0e99fabb9bb51ce1d5f5fdbc30b3671a61
linghengqian <linghengqian@outlook.com>
2023-01-28 01:00:21 +0800

    Prepare to publish elasticjob metadata

--- elasticjob-lite-core/src/test/java/com/lingh/util/ReflectionUtils.java
+++ elasticjob-lite-core/src/test/java/com/lingh/util/ReflectionUtils.java
@@ -6,6 +6,7 @@ import lombok.SneakyThrows;
 
 import java.lang.reflect.Field;
 
+@SuppressWarnings("unused")
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ReflectionUtils {
     @SneakyThrows
