74039859045436135aeb8fef73a2b97a30155a07
linghengqian <linghengqian@outlook.com>
2023-01-30 13:41:30 +0800

    Refactored unit tests for org.graalvm.js:js again, remove GPL V2 LICENSE dependency

--- js/src/test/java/com/lingh/App.java
+++ js/src/test/java/com/lingh/App.java
@@ -9,65 +9,67 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import java.io.IOException;
 
+@SuppressWarnings({"ThrowablePrintedToSystemOut", "unused", "RedundantThrows"})
 public class App {
 
     public static final int WARMUP = 30;
     public static final int ITERATIONS = 10;
     public static final String BENCHFILE = "src/bench.js";
 
-    public static final String SOURCE = ""
-            + "var N = 2000;\n"
-            + "var EXPECTED = 17393;\n"
-            + "\n"
-            + "function Natural() {\n"
-            + "    x = 2;\n"
-            + "    return {\n"
-            + "        'next' : function() { return x++; }\n"
-            + "    };\n"
-            + "}\n"
-            + "\n"
-            + "function Filter(number, filter) {\n"
-            + "    var self = this;\n"
-            + "    this.number = number;\n"
-            + "    this.filter = filter;\n"
-            + "    this.accept = function(n) {\n"
-            + "      var filter = self;\n"
-            + "      for (;;) {\n"
-            + "          if (n % filter.number === 0) {\n"
-            + "              return false;\n"
-            + "          }\n"
-            + "          filter = filter.filter;\n"
-            + "          if (filter === null) {\n"
-            + "              break;\n"
-            + "          }\n"
-            + "      }\n"
-            + "      return true;\n"
-            + "    };\n"
-            + "    return this;\n"
-            + "}\n"
-            + "\n"
-            + "function Primes(natural) {\n"
-            + "    var self = this;\n"
-            + "    this.natural = natural;\n"
-            + "    this.filter = null;\n"
-            + "\n"
-            + "    this.next = function() {\n"
-            + "        for (;;) {\n"
-            + "            var n = self.natural.next();\n"
-            + "            if (self.filter === null || self.filter.accept(n)) {\n"
-            + "                self.filter = new Filter(n, self.filter);\n"
-            + "                return n;\n"
-            + "            }\n"
-            + "        }\n"
-            + "    };\n"
-            + "}\n"
-            + "\n"
-            + "function primesMain() {\n"
-            + "    var primes = new Primes(Natural());\n"
-            + "    var primArray = [];\n"
-            + "    for (var i=0;i<=N;i++) { primArray.push(primes.next()); }\n"
-            + "    if (primArray[N] != EXPECTED) { throw new Error('wrong prime found: '+primArray[N]); }\n"
-            + "}\n";
+    public static final String SOURCE = """
+            var N = 2000;
+            var EXPECTED = 17393;
+
+            function Natural() {
+                x = 2;
+                return {
+                    'next' : function() { return x++; }
+                };
+            }
+
+            function Filter(number, filter) {
+                var self = this;
+                this.number = number;
+                this.filter = filter;
+                this.accept = function(n) {
+                  var filter = self;
+                  for (;;) {
+                      if (n % filter.number === 0) {
+                          return false;
+                      }
+                      filter = filter.filter;
+                      if (filter === null) {
+                          break;
+                      }
+                  }
+                  return true;
+                };
+                return this;
+            }
+
+            function Primes(natural) {
+                var self = this;
+                this.natural = natural;
+                this.filter = null;
+
+                this.next = function() {
+                    for (;;) {
+                        var n = self.natural.next();
+                        if (self.filter === null || self.filter.accept(n)) {
+                            self.filter = new Filter(n, self.filter);
+                            return n;
+                        }
+                    }
+                };
+            }
+
+            function primesMain() {
+                var primes = new Primes(Natural());
+                var primArray = [];
+                for (var i=0;i<=N;i++) { primArray.push(primes.next()); }
+                if (primArray[N] != EXPECTED) { throw new Error('wrong prime found: '+primArray[N]); }
+            }
+            """;
 
     public static void main(String[] args) throws Exception {
         benchGraalPolyglotContext();
