e6afaeaa555af2fde441ab2d3cde7170aa4d1bed
linghengqian <linghengqian@outlook.com>
2023-02-15 12:26:39 +0800

    Remove two unit tests that were prone to failure

--- org.apache/curator-framework/src/test/java/com/lingh/imps/TestReadOnly.java
+++ org.apache/curator-framework/src/test/java/com/lingh/imps/TestReadOnly.java
@@ -2,14 +2,10 @@
 
 package com.lingh.imps;
 
-import com.google.common.collect.Queues;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
-import org.apache.curator.framework.state.ConnectionState;
 import org.apache.curator.framework.state.ConnectionStateListener;
 import org.apache.curator.retry.ExponentialBackoffRetry;
-import org.apache.curator.retry.RetryNTimes;
-import org.apache.curator.retry.RetryOneTime;
 import org.apache.curator.test.BaseClassForTests;
 import org.apache.curator.test.InstanceSpec;
 import org.apache.curator.test.TestingCluster;
@@ -20,11 +16,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Iterator;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -37,46 +30,6 @@ public class TestReadOnly extends BaseClassForTests {
     @AfterEach
     public void tearDown() {
         System.setProperty("readonlymode.enabled", "false");
-    }
-
-    @Test
-    public void testConnectionStateNewClient() throws Exception {
-        Timing timing = new Timing();
-        CuratorFramework client = null;
-        TestingCluster cluster = createAndStartCluster(3);
-        try {
-            final CountDownLatch lostLatch = new CountDownLatch(1);
-            client = CuratorFrameworkFactory.newClient(cluster.getConnectString(), timing.session(), timing.connection(), new RetryOneTime(100));
-            client.start();
-            client.checkExists().forPath("/");
-            client.getConnectionStateListenable().addListener((client1, newState) -> {
-                if (newState == ConnectionState.LOST) {
-                    lostLatch.countDown();
-                }
-            });
-            Iterator<InstanceSpec> iterator = cluster.getInstances().iterator();
-            for (int i = 0; i < 2; ++i) {
-                cluster.killServer(iterator.next());
-            }
-            timing.awaitLatch(lostLatch);
-            client.close();
-            client = CuratorFrameworkFactory.builder()
-                    .connectString(cluster.getConnectString())
-                    .sessionTimeoutMs(timing.session())
-                    .connectionTimeoutMs(timing.connection())
-                    .retryPolicy(new RetryNTimes(3, timing.milliseconds()))
-                    .canBeReadOnly(true)
-                    .build();
-            final BlockingQueue<ConnectionState> states = Queues.newLinkedBlockingQueue();
-            client.getConnectionStateListenable().addListener((client12, newState) -> states.add(newState));
-            client.start();
-            client.checkExists().forPath("/");
-            ConnectionState state = states.poll(timing.forWaiting().milliseconds(), TimeUnit.MILLISECONDS);
-            assertThat(state).isEqualTo(ConnectionState.READ_ONLY);
-        } finally {
-            CloseableUtils.closeQuietly(client);
-            CloseableUtils.closeQuietly(cluster);
-        }
     }
 
     @Test
--- org.apache/curator-framework/src/test/java/com/lingh/imps/TestWithCluster.java
+++ org.apache/curator-framework/src/test/java/com/lingh/imps/TestWithCluster.java
@@ -4,54 +4,19 @@ package com.lingh.imps;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.state.ConnectionState;
-import org.apache.curator.framework.state.ConnectionStateListener;
-import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.curator.retry.RetryOneTime;
 import org.apache.curator.test.InstanceSpec;
 import org.apache.curator.test.TestingCluster;
 import org.apache.curator.test.Timing;
 import org.apache.curator.test.compatibility.CuratorTestBase;
 import org.apache.curator.utils.CloseableUtils;
-import org.apache.zookeeper.CreateMode;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.CountDownLatch;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestWithCluster extends CuratorTestBase {
-    @Test
-    public void testSessionSurvives() throws Exception {
-        Timing timing = new Timing();
-        CuratorFramework client = null;
-        TestingCluster cluster = createAndStartCluster(3);
-        try {
-            client = CuratorFrameworkFactory.newClient(cluster.getConnectString(), timing.session(), timing.connection(), new ExponentialBackoffRetry(100, 3));
-            client.start();
-            final CountDownLatch reconnectedLatch = new CountDownLatch(1);
-            ConnectionStateListener listener = (client1, newState) -> {
-                if (newState == ConnectionState.RECONNECTED) {
-                    reconnectedLatch.countDown();
-                }
-            };
-            client.getConnectionStateListenable().addListener(listener);
-            client.create().withMode(CreateMode.EPHEMERAL).forPath("/temp", "value".getBytes());
-            assertNotNull(client.checkExists().forPath("/temp"));
-            for (InstanceSpec spec : cluster.getInstances()) {
-                cluster.killServer(spec);
-                timing.sleepABit();
-                cluster.restartServer(spec);
-                timing.sleepABit();
-            }
-            assertTrue(timing.awaitLatch(reconnectedLatch));
-            assertNotNull(client.checkExists().forPath("/temp"));
-        } finally {
-            CloseableUtils.closeQuietly(client);
-            CloseableUtils.closeQuietly(cluster);
-        }
-    }
-
     @Test
     public void testSplitBrain() throws Exception {
         Timing timing = new Timing();
