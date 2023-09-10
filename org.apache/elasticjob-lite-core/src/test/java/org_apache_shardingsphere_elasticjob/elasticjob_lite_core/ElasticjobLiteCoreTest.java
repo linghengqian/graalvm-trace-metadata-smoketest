c6c8b4f3c4a5de60848b956cb64a4cab0ed5608d
linghengqian <linghengqian@outlook.com>
2023-09-10 22:58:26 +0800

    Refactor the metadata and unit tests of `org.apache.shardingsphere.elasticjob:elasticjob-lite-core:3.0.3` again

--- org.apache/elasticjob-lite-core/src/test/java/org_apache_shardingsphere_elasticjob/elasticjob_lite_core/ElasticjobLiteCoreTest.java
+++ org.apache/elasticjob-lite-core/src/test/java/org_apache_shardingsphere_elasticjob/elasticjob_lite_core/ElasticjobLiteCoreTest.java
@@ -4,16 +4,10 @@ import org.apache.curator.CuratorZookeeperClient;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.curator.test.TestingServer;
 import org.apache.shardingsphere.elasticjob.api.JobConfiguration;
-import org.apache.shardingsphere.elasticjob.api.ShardingContext;
-import org.apache.shardingsphere.elasticjob.dataflow.job.DataflowJob;
-import org.apache.shardingsphere.elasticjob.dataflow.props.DataflowJobProperties;
-import org.apache.shardingsphere.elasticjob.http.props.HttpJobProperties;
 import org.apache.shardingsphere.elasticjob.lite.api.bootstrap.impl.OneOffJobBootstrap;
-import org.apache.shardingsphere.elasticjob.lite.api.bootstrap.impl.ScheduleJobBootstrap;
 import org.apache.shardingsphere.elasticjob.reg.base.CoordinatorRegistryCenter;
 import org.apache.shardingsphere.elasticjob.reg.zookeeper.ZookeeperConfiguration;
 import org.apache.shardingsphere.elasticjob.reg.zookeeper.ZookeeperRegistryCenter;
-import org.apache.shardingsphere.elasticjob.script.props.ScriptJobProperties;
 import org.apache.shardingsphere.elasticjob.simple.job.SimpleJob;
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.AfterAll;
@@ -21,19 +15,10 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
-import org_apache_shardingsphere_elasticjob.elasticjob_lite_core.entity.TOrderPOJO;
 import org_apache_shardingsphere_elasticjob.elasticjob_lite_core.repository.TOrderRepository;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.attribute.PosixFilePermission;
 import java.time.Duration;
-import java.util.List;
-import java.util.Locale;
-import java.util.Objects;
-import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -59,11 +44,8 @@ class ElasticjobLiteCoreTest {
     }
 
     @AfterAll
-    static void afterAll() {
-        Awaitility.await().atMost(Duration.ofSeconds(5)).until(() -> {
-            testingServer.close();
-            return true;
-        });
+    static void afterAll() throws IOException {
+        testingServer.close();
     }
 
     @Test
@@ -98,17 +80,5 @@ class ElasticjobLiteCoreTest {
         );
         result.init();
         return result;
-    }
-
-    private String getMultipleOSShellCommand() throws IOException {
-        if (System.getProperties().getProperty("os.name").toLowerCase(Locale.ENGLISH).contains("win")) {
-            return Paths.get(Objects.requireNonNull(getClass().getResource("/script/demo.bat")).getPath().substring(1)).toString();
-        }
-        Path result = Paths.get(Objects.requireNonNull(getClass().getResource("/script/demo.sh")).getPath());
-        Files.setPosixFilePermissions(result, Set.of(PosixFilePermission.OWNER_READ, PosixFilePermission.OWNER_WRITE, PosixFilePermission.OWNER_EXECUTE,
-                PosixFilePermission.GROUP_READ, PosixFilePermission.GROUP_EXECUTE,
-                PosixFilePermission.OTHERS_READ, PosixFilePermission.OTHERS_EXECUTE
-        ));
-        return result.toString();
     }
 }
